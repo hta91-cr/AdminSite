6f249818dcebc5d37fc0e582303a9c1850b18657
hta91-cr <hta200891@gmail.com>
2020-01-27 21:52:01 -0600

    Primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from '../menu/menu/menu.component';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { BodyComponent } from '../core/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModulesPro.forRoot(),
+
   ],
-  providers: [],
+  providers: [MDBSpinningPreloader],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/core/body/body.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { IMyOptions } from 'ng-uikit-pro-standard';
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.scss']
+})
+export class BodyComponent implements OnInit {
+  public chartType: string = 'line';
+  public myDatePickerOptions: IMyOptions = {
+    // Your options
+  };
+  public chartDatasets: Array<any> = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Time' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Ammount' }
+  ];
+  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(29, 136, 218, .4)',
+      borderColor: 'rgba(29, 136, 218, .7)',
+      borderWidth: 2,
+      pointRadius: 0
+    },
+    {
+      backgroundColor: 'rgba(23, 102, 162, .4)',
+      borderColor: 'rgba(23, 102, 162, .7)',
+      borderWidth: 2,
+      pointRadius: 0
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true,
+
+  };
+  public optionsSelect: Array<any>;
+
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
+  constructor() { }
+
+  ngOnInit() {
+    this.optionsSelect = [
+      { value: '1', label: 'Today' },
+      { value: '2', label: 'Last 7 days' },
+      { value: '3', label: 'Last 30 days' },
+    ];
+  }
+
+  updateOnlyDatasets() {
+    const firstChartData = [
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+    ];
+    const secondChartData = [
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+    ];
+    // This line will update only data in your Chart
+    this.chartDatasets = [
+      { data: firstChartData },
+      { data: secondChartData },
+    ];
+  }
+
+}
